5b7a332cbedadd9ea012d1396619b087c3ce6220
wenchen1114 <54914849+wenchen1114@users.noreply.github.com>
2020-10-25 23:44:25 -0400

    Add Undo

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let ctx = canvas.getContext("2d");
 let dit = document.getElementById("dit");
 let dah = document.getElementById("dah");
 let space = document.getElementById("space");
+let undo = document.getElementById("undo");
 
 let last_x = 0;
 let last_y = 0;
@@ -25,6 +26,7 @@ let penDown = false;
 let colors = ["rgb(255, 247, 218)", "rgb(252, 255, 236", "rgb(224, 224, 224)", "rgb(245, 255, 255)", "rgb(224, 246, 255)"];
 let word = "";
 let words = [];
+let undoStack = [];
 
 
 canvas.width = window.innerWidth;
@@ -33,7 +35,6 @@ canvas.height = window.innerHeight;
 ctx.fillStyle = "rgb(255, 252, 243)";
 
 make_base();
-
 function make_base(){
     let base_image = new Image();
     base_image.src = 'img/bg.png';
@@ -49,33 +50,37 @@ function paintStart(x, y){
 }
 
 function paintMove(x, y){
+    ctx.shadowColor = 'white';
+    ctx.shadowBlur = 15;
     ctx.beginPath();
     let num = Math.floor(Math.random()* 5);
     ctx.fillStyle = colors[num];
-    ctx.arc(last_x, last_y, 5, 0, Math.PI*2);
+    ctx.arc(last_x, last_y, Math.floor(Math.random()* 5)+1, 0, Math.PI*2);
     ctx.fill();
     last_x = x;
     last_y = y;
     word += ".";
+    undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
 }
 
 function paintMove2(x, y){
+    ctx.shadowColor = 'white';
+    ctx.shadowBlur = 15;
     ctx.beginPath();
     let num = Math.floor(Math.random()* 5);
     ctx.fillStyle = colors[num];
-    ctx.fillRect(last_x-10, last_y+2.5, 20, 5);
+    ctx.fillRect(last_x-10, last_y+2.5, Math.floor(Math.random()* 10)+10, 5);
     ctx.fill();
     last_x = x;
     last_y = y;
     word += "-";
+    undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
 }
 
-let f = new FontFace('Press Start 2P', 'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
-
-f.load().then(function() {
-});
 
 function paintText(word){
+    ctx.shadowColor = 'white';
+    ctx.shadowBlur = 0;
     for(let i = 0; i<morsecode.length; i++){
         if(word == morsecode[i]){
             words.push(abc[i]);
@@ -85,7 +90,7 @@ function paintText(word){
     ctx.fillStyle = "rgb(255, 252, 243)";
     ctx.font = '16px "Press Start 2P"';
     ctx.fillText(sentence, 10, 100);
-    console.log(words);
+    undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
 }
 
 canvas.addEventListener("mousedown", function(evt){
@@ -134,4 +139,13 @@ space.addEventListener("click", function(){
     console.log(word);
     paintText(word);
     word = "";
+})
+
+undo.addEventListener("click", function(){
+    words.pop();
+    if(undoStack.length > 1){
+        undoStack.pop();
+    }
+    let lastStack = undoStack[undoStack.length -1];
+    ctx.putImageData(lastStack, 0, 0);
 })
